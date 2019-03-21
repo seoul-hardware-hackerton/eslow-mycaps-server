4f63ba71e9f14963059a29023d156264974a9547
LenKIM <joenggyu0@gmail.com>
2019-03-21 15:14:58 +0900

    feat: add MQTT protocal

--- src/main/java/com/seoulhackerton/mycaps/MycapsApplication.java
+++ src/main/java/com/seoulhackerton/mycaps/MycapsApplication.java
@@ -1,13 +1,22 @@
 package com.seoulhackerton.mycaps;
 
+import com.seoulhackerton.mycaps.hello.SimpleMqttCallBack;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MycapsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MqttException {
+        System.out.println("== START SUBSCRIBER ==");
+
+        MqttClient client = new MqttClient("tcp://52.141.36.28:1883", MqttClient.generateClientId());
+        client.setCallback(new SimpleMqttCallBack());
+        client.connect();
+        client.subscribe("eslow");
+
         SpringApplication.run(MycapsApplication.class, args);
     }
-
 }
