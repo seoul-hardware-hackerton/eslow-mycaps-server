632fd91636cbc609abf05780ab46025a693ad297
LenKIM <joenggyu0@gmail.com>
2019-03-30 01:48:54 +0900

    test: mqtt protocal

--- src/main/java/com/seoulhackerton/mycaps/MycapsApplication.java
+++ src/main/java/com/seoulhackerton/mycaps/MycapsApplication.java
@@ -3,6 +3,7 @@ package com.seoulhackerton.mycaps;
 import com.seoulhackerton.mycaps.components.mqtt.audioWavCallback;
 import com.seoulhackerton.mycaps.components.mqtt.voiceLevelCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,8 +29,17 @@ public class MycapsApplication {
     private static void voiceLevelSubscribe() throws MqttException {
         System.out.println("== START Voice Level SUBSCRIBER ==");
         MqttClient client = new MqttClient("tcp://52.141.36.28:1883", MqttClient.generateClientId());
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        client.connect(options);
         client.setCallback(new voiceLevelCallback());
-        client.connect();
         client.subscribe("eslow/voice");
     }
 }
+
+//subscriber.subscribe(EngineTemperatureSensor.TOPIC, (topic, msg) -> {
+//        byte[] payload = msg.getPayload();
+//        log.info("[I46] Message received: topic={}, payload={}", topic, new String(payload));
+//        receivedSignal.countDown();
+//        });
