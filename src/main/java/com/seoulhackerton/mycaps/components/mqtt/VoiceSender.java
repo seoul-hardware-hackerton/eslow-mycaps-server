582b8bd1c7c276467e50b11aefaaafd9bd4dbca7
LenKIM <joenggyu0@gmail.com>
2019-03-31 11:09:50 +0900

    fix: test code

---
+++ src/main/java/com/seoulhackerton/mycaps/components/mqtt/VoiceSender.java
@@ -0,0 +1,55 @@
+package com.seoulhackerton.mycaps.components.mqtt;
+
+import com.seoulhackerton.mycaps.Constant;
+import com.seoulhackerton.mycaps.service.MqttPublishClient;
+import com.seoulhackerton.mycaps.service.MqttPublishClient2;
+import com.seoulhackerton.mycaps.service.SpeechRecognitionSamples;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+
+public class VoiceSender {
+
+    private static ConcurrentLinkedQueue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<String>();
+    boolean isRunning = false;
+
+    public VoiceSender() {
+        new Thread(() -> {
+            isRunning = true;
+            MqttPublishClient client = new MqttPublishClient();
+            while (isRunning) {
+                setSpeechRecognitionSamples(client);
+            }
+        }).start();
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+
+    private void normalAlarm(MqttPublishClient client) {
+        client.send(Constant.ALARM_MQTT_TOPIC, String.valueOf(Constant.NONE));
+    }
+
+    private void sendAlarm(MqttPublishClient client) {
+        client.send(Constant.ALARM_MQTT_TOPIC, String.valueOf(Constant.MBED_BEEF_SOUND));
+    }
+
+
+    public void setSpeechRecognitionSamples(MqttPublishClient client) {
+        while (!concurrentLinkedQueue.isEmpty()) {
+            String filePath = concurrentLinkedQueue.poll();
+            Double a = Double.parseDouble(filePath);
+            if (a >= 1) {
+                sendAlarm(client);
+            } else {
+                normalAlarm(client);
+            }
+        }
+    }
+
+    public void add(String fileName) {
+        concurrentLinkedQueue.add(fileName);
+    }
+}
--- src/main/java/com/seoulhackerton/mycaps/components/mqtt/voiceLevelCallback.java
+++ src/main/java/com/seoulhackerton/mycaps/components/mqtt/voiceLevelCallback.java
@@ -1,5 +1,6 @@
 package com.seoulhackerton.mycaps.components.mqtt;
 
+import com.seoulhackerton.mycaps.AlarmSender;
 import com.seoulhackerton.mycaps.Constant;
 import com.seoulhackerton.mycaps.service.MqttPublishClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -8,33 +9,19 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class voiceLevelCallback implements MqttCallback {
 
+    private VoiceSender alarmSender;
+
+    public voiceLevelCallback(VoiceSender alarmSender) {
+        this.alarmSender = alarmSender;
+    }
+
     public void connectionLost(Throwable throwable) {
         System.out.println("Connection to MQTT broker lost!");
     }
 
     public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-        MqttPublishClient client = new MqttPublishClient();
-//        byte[] voiceLevel = mqttMessage.getPayload();
-        Double a =  Double.parseDouble(new String(mqttMessage.getPayload()));
-                    if (a >= 1) {
-                sendAlarm(client);
-            } else {
-                normalAlarm(client);
-            }
-
+        alarmSender.add(new String(mqttMessage.getPayload()));
         System.out.println("Message received:\n\t" + new String(mqttMessage.getPayload()));
-
-//        for (byte b : voiceLevel) {
-//            double a = (b) - 48;
-//        }
-    }
-
-    private void normalAlarm(MqttPublishClient client) {
-        client.send(Constant.ALARM_MQTT_TOPIC, String.valueOf(Constant.NONE));
-    }
-
-    private void sendAlarm(MqttPublishClient client) {
-        client.send(Constant.ALARM_MQTT_TOPIC, String.valueOf(Constant.MBED_BEEF_SOUND));
     }
 
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
