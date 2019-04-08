f85548ae517004127f0e23e35cff4bde91afcab6
leekhee <leekhee91@naver.com>
2019-04-09 02:34:02 +0900

    Update MessageService.java
    Token바꿈

--- src/main/java/com/seoulhackerton/mycaps/service/telegram/MessageService.java
+++ src/main/java/com/seoulhackerton/mycaps/service/telegram/MessageService.java
@@ -13,7 +13,7 @@ public class MessageService {
 
     public String sendMsg(String text) {
 
-        String url = "https://api.telegram.org/bot818348795:AAE3-dC2J1POYDmss1JZHURDgP_R5wqx4m0/sendMessage?chat_id=727848241&text=";
+        String url = "https://api.telegram.org/botToken/sendMessage?chat_id=727848241&text=";
         String msg = url + URLEncoder.encode(text);
         coreTelegramService.sendMsg(msg);
         return text;
